b6d52f2c969ea2792ac680212c0dc52d02224661
Hannes <hannes@octofox.de>
2020-05-29 17:59:04 +0200

    :sparkles: Implement first stage suffix sort

--- src/main/java/de/octofox/suffixarray/SuffixArray.java
+++ src/main/java/de/octofox/suffixarray/SuffixArray.java
@@ -1,92 +1,127 @@
 package de.octofox.suffixarray;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class SuffixArray {
     private final String text;
-
-    private Entry[] L;
-
-    /**
-     * P contains all sorted buckets after H stages
-     * Entries represent Buckets in each H step
-     * Each Step should be equal to the Pos array described in page 5 (first line)
-     * "At the start of stage H, Pos [i] contains the start position of the irh smallest suffix"
-     */
-    private int[][] P;
+    private final char[] textChars;
 
     /**
-     * Length of the text
+     * Holds the suffix at a given position
+     * Sorted lexicographically
      */
-    private int N;
+    private final int[] suffixRank;
 
-    private class Entry {
-        private int[] nr;
-        private int p;
+    /**
+     * Holds the rank of a given suffix position
+     * E.g.: rankOfSuffix[ SuffixPos ] = Rank
+     */
+    private final int[] rankOfSuffix;
 
-        public Entry(int[] nr, int p) {
-            this.nr = nr;
-            this.p = p;
-        }
-    }
+    /**
+     * Value is true if the suffix is the lexicographically smallest in its bucket, false otherwise
+     */
+    private final boolean[] smallestSuffixInBucket;
+
 
     public SuffixArray(final String text) {
         this.text = text;
+        this.textChars = text.toCharArray();
 
-        this.N = text.length();
+        suffixRank = new int[text.length()];
+        rankOfSuffix = new int[text.length()];
+        smallestSuffixInBucket = new boolean[text.length()];
 
-        this.L = new Entry[N];
+        firstStageSort();
 
-        // log steps X text length
-        // The sorting is done in log2(N+1) stages
-        // Paper Page 4. Chapter 3. Sorting. First line
-        this.P = new int[(int) Math.log((N + 1))][N];
-
-        // TODO not tested
-        this.sort();
-    }
-
-    /**
-     * Compares two entries
-     *
-     * @param a First Entry
-     * @param b Second Entry
-     * @return 1 if a is smaller than b
-     */
-    private int cmp(Entry a, Entry b) {
-        return a.nr[0] == b.nr[0] ? (a.nr[1] < b.nr[1] ? 1 : 0) : (a.nr[0] < b.nr[0] ? 1 : 0);
-    }
-
-    private void sort() {
-        int stp;
-        int cnt;
-
-        // First Stage
-        // The first stage consists of a bucket sort according to the first symbol of each suffix.
-        // Paper Page 4. Chapter 3. Sorting
-        for (int i = 0; i < N; i++) {
-            P[0][i] = this.text.charAt(i) - 'a';
-        }
-
-        // Further Sorting Stages
-        // Each stage doubles the length of compared suffixes
-        for (stp = 1, cnt = 1; cnt >> 1 < N; stp++, cnt <<= 1) {
-            // TODO
-            for (int i = 0; i < N; i++) {
-                L[i].nr[0] = P[stp - 1][i];
-                L[i].nr[1] = (i + cnt < N) ? P[stp - 1][i + cnt] : -1;
-                L[i].p = i;
-            }
-
-            Arrays.sort(L, this::cmp);
-
-            // TODO
-            for (int i = 0; i < N; i++) {
-                P[stp][L[i].p] =
-                        i > 0 &&
-                                L[i].nr[0] == L[i - 1].nr[0] &&
-                                L[i].nr[1] == L[i - 1].nr[1] ? P[stp][L[i - 1].p] : i;
-            }
+        System.out.println("i | Bh | sufinv | suftab | suffix");
+        for (int i = 0; i < text.length(); i++) {
+            System.out.println(i + " | " + (smallestSuffixInBucket[i] ? 1 : 0)  + "  | " + rankOfSuffix[i] + "      | " + suffixRank[i] + "      | " + text.substring(suffixRank[i]));
         }
     }
+
+    private void firstStageSort() {
+        // Mapping from Character to bucket size
+        Map<Character, Integer> alphabet = new HashMap<>();
+
+        // First iterate over all characters of the text
+        for (int i = 0; i < textChars.length; i++) {
+            final char currentChar = textChars[i];
+
+            // If alphabet contains the current character increment the size of the bucket
+            // E.g.: input 'abaccc'
+            // => Bucket size for char 'a' = 2
+            // => Bucket size for char 'b' = 1
+            // => Bucket size for char 'c' = 3
+            if (alphabet.containsKey(currentChar)) {
+                int bucketSize = alphabet.get(currentChar);
+
+                bucketSize++;
+
+                alphabet.put(currentChar, bucketSize);
+            } else {
+                // Else set bucket size to 1
+                alphabet.put(currentChar, 1);
+            }
+
+            // Base value initialization
+            suffixRank[i] = -1;
+            smallestSuffixInBucket[i] = false;
+            //B2H[t] = false;
+            //Count[t] = 0;
+
+        }
+
+        int currentSuffix = 0;
+
+        // First stage sort comparing the first character of each suffix
+        for (char x : textChars) {
+            // Start position of a bucket in the suffix array
+            int positionOfStartOfBucket = 0;
+            // First free position in a bucket
+            int offset;
+
+            // For each character of the text iterate over the alphabet
+            for (Map.Entry<Character, Integer> entry: alphabet.entrySet()) {
+                // If text character does not match the current alphabet character:
+                // add the size of the characters bucket to the current offset
+                // Character 'a' will typically start at offset 0
+                // Character 'b' will start at 0 + size of bucket for 'a'
+                // Character 'c' will start at 0 + size of bucket for 'a' + size of bucket for 'b'
+                // ...
+                if (x != entry.getKey()) {
+                    int bucketSizeOffset = entry.getValue();
+                    positionOfStartOfBucket += bucketSizeOffset;
+                } else {
+                    // Characters match
+                    // first position of corresponding bucket has been computed
+                    break;
+                }
+            }
+
+            // positionOfStartOfBucket will always be first position of bucket
+            offset = positionOfStartOfBucket;
+
+            // Mark the position as the start of the bucket
+            smallestSuffixInBucket[positionOfStartOfBucket] = true;
+
+            // Check if the current index is already in use
+            // If so increment offset by one
+            while (suffixRank[offset] != -1) {
+                offset++;
+            }
+
+            // Set the rank of the current suffix
+            // POS array in Manber Myers Algorithm
+            // Mapping from Rank => Suffix => Suffix_i has the lexicographic rank of X
+            suffixRank[offset] = currentSuffix;
+
+            // Set the inverse array
+            // PRM array in Manber Myers
+            // Mapping from Suffix to Rank => Suffix_i has Rank of X
+            rankOfSuffix[currentSuffix] = offset;
+            currentSuffix++;
+        }
+    }
+
 }
