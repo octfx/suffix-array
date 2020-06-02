f5907ec3eddb72cec4fc3681c636a6261e20168d
Hannes <hannes@octofox.de>
2020-06-02 11:16:19 +0200

    :sparkles: Add method for reading files
    Set java compile version to 8

--- src/main/java/de/octofox/suffixarray/App.java
+++ src/main/java/de/octofox/suffixarray/App.java
@@ -1,7 +1,54 @@
 package de.octofox.suffixarray;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class App {
     public static void main(String[] args) {
-        (new SuffixArray("acbaacatat")).compute();
+        final String input = parseArgs(args);
+
+        if (input.length() > 0) {
+            (new SuffixArray(input)).compute();
+        } else {
+            System.out.println("Call jar with '-f filename' to use a local file to sort. Call with one arg: Argument is used as input string.");
+        }
+    }
+
+    /**
+     * Simple method to parse given jvm arguments
+     *
+     * @param args CLI args '-f filename' or 'StringToSort'
+     * @return String to sort
+     */
+    private static String parseArgs(final String[] args) {
+        if (args == null || args.length == 0 || args.length > 2) {
+            return "";
+        }
+
+        if (args.length == 1) {
+            return args[0];
+        }
+
+        return readFile(args[1]);
+    }
+
+    /**
+     * Reads a file to string
+     *
+     * @param fileName The file to read
+     * @return File as string
+     */
+    private static String readFile(final String fileName) {
+        File file = new File(fileName);
+
+        try {
+            byte[] fileBytes = Files.readAllBytes(file.toPath());
+            return new String(fileBytes);
+        } catch (IOException e) {
+            System.out.println("Could not read file '" + fileName + "'." + e.getMessage());
+        }
+
+        return "";
     }
 }
